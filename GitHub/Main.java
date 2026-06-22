a37e972edfbd06c12bf2ed79dc2f7ff1a25e7bba
Akash242005 <akashb2403@gmail.com>
2026-06-22 20:46:58 +0530

    Changeed

--- GitHub/Main.java
+++ GitHub/Main.java
@@ -1,5 +1,18 @@
+class Student{
+    int age;
+    String name;
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+}
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello akash!!");
+        Student s1 = new Student(12, "AKASH");
+        Student s2 = s1;
+        System.out.println(s2.name+" "+s2.age);
+        s2.name = "GAN";
+        System.out.println(s1.name+" "+s2.age);
     }
 }
