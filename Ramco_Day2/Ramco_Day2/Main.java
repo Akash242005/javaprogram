ef0701165cb27315a78778b2912d40d3d24184ea
Akash242005 <akashb2403@gmail.com>
2026-06-23 16:00:23 +0530

    I have changed

--- Ramco_Day2/Ramco_Day2/Main.java
+++ Ramco_Day2/Ramco_Day2/Main.java
@@ -2,7 +2,6 @@
 // Static Factory Methods 
 public class Main{
     public static void main(String[] args) {
-        int r = 10;
         double PI = 3.14;
         double vol = (double)4/3 * PI * r * r* r;
         System.out.println(vol);
